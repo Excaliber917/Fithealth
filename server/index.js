ed78c109213437e63b8900e0e08b2fc8ede0766a
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2024-08-31 01:27:45 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDb } from './db/connectDb.js'
+import authRoute from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
+import userRoute from './routes/user.route.js';
+import messageRouter from './routes/message.route.js';
+dotenv.config()
+const app = express()
+app.use(express.json());
+app.use(cookieParser())
+const port = process.env.PORT || 5001
+
+app.use("/api/auth",authRoute)
+app.use("/api/user",userRoute)
+app.use("/api/mentalhealth",messageRouter)
+
+
+
+app.listen(port,()=>{
+    connectDb()
+    console.log(`app is running on ${port} `)
+})
